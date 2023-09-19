45c3b173672cc1e2ab92fcf6653aa5343403ed88
Md Shamim <shamimice03@gmail.com>
2023-09-19 17:02:57 +0900

    Create grafana_setup.sh

---
+++ installation/grafana_setup.sh
@@ -0,0 +1,7 @@
+```bash
+sudo yum update -y
+sudo yum install -y https://dl.grafana.com/enterprise/release/grafana-enterprise-10.0.0-1.x86_64.rpm
+sudo systemctl start grafana-server
+sudo systemctl enable grafana-server
+sudo systemctl status grafana-server
+```
